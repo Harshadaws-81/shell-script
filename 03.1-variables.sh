34591456de650bfa4ea4428ee8da0e258f0d6120
harshavardhan <kadiamharsha27@gmail.com>
2024-08-30 11:06:48 +0530

    variables with corrections made

---
+++ 03.1-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=Swathi
+PERSON2=Pruthvi
+
+echo "$PERSON1:: Hi ${PERSON2} , How are you ?"
+echo "${PERSON2}:: Hello $PERSON1. I am Fabulous . How are you doing?"
+echo "$PERSON1:: I am doing great ${PERSON2}. What's going on?"
+echo "${PERSON2}:: I started learning Shell Script $PERSON1"
+
+echo  "please save "

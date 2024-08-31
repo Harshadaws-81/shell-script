a10e6d6f83d44ae6d53b786dee0da44434de128d
harshavardhan <kadiamharsha27@gmail.com>
2024-08-31 15:20:36 +0530

    script for condition

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#NUMBER=$1
-#if [ $NUMBER -gt 20 ] # gt(>)  lt(<)  eq(=)  -ne(!=)
-#then 
-#   echo "Given number: $NUMBER is greater than 20"
-#else
-#    echo "Given number : $NUMBER is less than 20"
-#fi
-
-
 NUMBER=$1
-if[ $NUMBER -eq 18 ]
-then
-    echo "Given number : $NUMBER is equal to 18"
+if [ $NUMBER -lt 20 ] # gt(>)  lt(<)  eq(=)  -ne(!=)
+then 
+   echo "Given number: $NUMBER is less than 20"
 else
-    echo "Given number : $NUMBER is not equal to 18"
+    echo "Given number : $NUMBER is greater than 20"
 fi
+
+
+

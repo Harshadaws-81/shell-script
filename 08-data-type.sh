961b9c051493dacce52f5a8c7bce8f185ecf1107
harshavardhan <kadiamharsha27@gmail.com>
2024-08-31 13:38:36 +0530

    using data-types ; sum

--- 08-data-type.sh
+++ 08-data-type.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# data-types
 
 NUMBER1=$1
 NUMBER2=$2
 
-SUM=$(($NUMBER1+$NUMBER2))
+MULTIPLICATION=$(($NUMBER1*$NUMBER2))
 
-echo "Total of $NUMBER1 and $NUMBER2 is : $SUM"
+echo "Total of $NUMBER1 and $NUMBER2 is : $MULTIPLICATION"

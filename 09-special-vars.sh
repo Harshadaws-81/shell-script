d67327b5df201f576a40289ffeb9971ea0876e26
harshavardhan <kadiamharsha27@gmail.com>
2024-08-31 14:27:49 +0530

    knowing the special variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -6,6 +6,8 @@ echo "All Variables passed to the script :$@"
 echo "Number of variables passed : $#"
 echo "Script Name :$0"
 echo "Current Working Directory : $PWD"
-echo "Home Directory of the current User : $HOME"
+echo "Home Directory of the current User : $HOME
+echo "PID of the script executing now : $$"
+echo "PID of the last background command : $!"
 
 

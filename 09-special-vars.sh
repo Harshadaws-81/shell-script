3ce8302fce3101f5e51924f622334cc2bdc762d3
harshavardhan <kadiamharsha27@gmail.com>
2024-08-31 13:58:56 +0530

    knowing the special variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+#echo "little more practice is needed"
 # reflect the corrections made
-echo "All Variables passed to the script :$@"
 
-echo "little more practice is needed"
+echo "All Variables passed to the script :$@"
+echo "Number of variables passed : $#"
+
+

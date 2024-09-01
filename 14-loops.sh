2d43053f26ed540d430e6a8d1917a893a9cb44c7
harshavardhan <kadiamharsha27@gmail.com>
2024-09-01 17:44:03 +0530

    Usage of Loops in scripting

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# basic script in shell can be modified
+# for i in 1 2 3 4 5 6 7 8 9 
+# do
+#     echo $i
+# done
 
-for i in 1 2 3 4 5 6 7 8 9 
+for i in {0..27}
 do
     echo $i
 done

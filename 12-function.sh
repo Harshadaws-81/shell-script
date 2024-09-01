b140577fedc8bada6f7a136b7ed043e66065af1f
harshavardhan <kadiamharsha27@gmail.com>
2024-09-01 14:01:35 +0530

    FUNCTIONS using the script

--- 12-function.sh
+++ 12-function.sh
@@ -6,10 +6,10 @@ USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "Command is ... FAILED"
+        echo "$2 is ... FAILED"
         exit 1
     else 
-        echo "Command is ... SUCCESS"
+        echo "$2 is ... SUCCESS"
     fi
 }
 
@@ -21,7 +21,7 @@ fi
 
 dnf list installed git 
 
-VALIDATE $?
+VALIDATE $? "Listing Git "
 
 # if [ $? -ne 0 ]
 # then

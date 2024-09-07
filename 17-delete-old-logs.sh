e8bc02cc31dec62eed7c66ca8e2664d01b4cdde0
harshavardhan <kadiamharsha27@gmail.com>
2024-09-07 11:53:45 +0530

    Deleting old logs - script

---
+++ 17-delete-old-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SOURCE_DIR=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR $G exists $N"
+else
+    echo "$SOURCE_DIR $R doesnot exist $N"
+fi

3190eb6323404c0d94892b7f3ff995e1e4f4e25d
harshavardhan <kadiamharsha27@gmail.com>
2026-03-13 08:33:29 +0530

    Variables-with args usage

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,3 +6,5 @@ echo "$PERSON1:: Hi ${PERSON2} , How are you ?"
 echo "${PERSON2}:: Hello $PERSON1. I am Fabulous . How are you doing?"
 echo "$PERSON1:: I am doing great ${PERSON2}. What's going on?"
 echo "${PERSON2}:: I started learning Shell Script $PERSON1"
+
+# variables with arguments passed from command line.

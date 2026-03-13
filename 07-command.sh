a41d88036907a6fdc7f6df5fc37d313774366498
harshavardhan <kadiamharsha27@gmail.com>
2026-03-13 08:50:47 +0530

    command

--- 07-command.sh
+++ 07-command.sh
@@ -3,3 +3,5 @@
 DATE=$(date +%F)
 
 echo "Today's Date is : $DATE"
+
+# COMMAND - we can keep the command inside $() and the output of the command will be stored in the variable. we can use that variable where ever we want.
